07e183d7bb600d2581fde3080a270356bd4ab5c9
rubicus3 <laba625@yandex.ru>
2025-05-19 11:08:31 +0300

    rename file

--- app/src/main/java/com/rubicus/keymechapp/SwtichItemCard.java
+++ app/src/main/java/com/rubicus/keymechapp/SwtichItemCard.java
@@ -59,6 +59,6 @@ public class SwtichItemCard extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_item_swtich_card, container, false);
+        return inflater.inflate(R.layout.fragment_swtich_item_card, container, false);
     }
 }

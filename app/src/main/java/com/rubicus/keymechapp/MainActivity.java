bec66548508f4e3a092247f4765db12b571071b2
rubicus3 <laba625@yandex.ru>
2025-05-22 02:34:28 +0300

    AAAAAAAAAAAAAAAAa

--- app/src/main/java/com/rubicus/keymechapp/MainActivity.java
+++ app/src/main/java/com/rubicus/keymechapp/MainActivity.java
@@ -1,11 +1,14 @@
 package com.rubicus.keymechapp;
 
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.FragmentManager;
 
@@ -20,9 +23,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0);
             return insets;
         });
+
         FragmentManager fragmentManager = getSupportFragmentManager();
         BottomNavigationView bottomNavigationView = findViewById(R.id.nav_menu);
         bottomNavigationView.setOnItemSelectedListener(menuItem -> {
